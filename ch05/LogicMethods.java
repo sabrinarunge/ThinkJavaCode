c26f5205bdf6baeaeb1ab0c790e4ba93c78684ba
Sabrina Runge <thesabrinarunge@gmail.com>
2018-07-27 14:02:51 -0500

    Logic Methods

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -6,6 +6,7 @@ public class LogicMethods
         printIsLarge(num);
         printIsLargeOrSmall(num);
         printLargest(2, 4);
+        printLargestOdd(3, 5);
     }
 
     public static void printIsLarge(int number)
@@ -48,6 +49,29 @@ public class LogicMethods
         else if (number1 == number2)
         {
             System.out.println("The numbers are equal");
+        }
+    }
+
+    public static void printLargestOdd(int number1, int number2)
+    {
+        if (number1 > number2 && number1 % 2 != 0)
+        {
+            System.out.println("The largest odd number is: " + number1);
+        }
+
+        else if (number2 > number1 && number2 % 2 != 0)
+        {
+            System.out.println("The largest odd number is: " + number2);
+        }
+
+        else if (number1 % 2 == 0 && number2 % 2 == 0)
+        {
+            System.out.println("Neither number is odd");
+        }
+
+        if (number1 % 2 != 0 && number2 % 2 != 0)
+        {
+            System.out.println("Two odds make an even. Total: " + (number1 + number2));
         }
     }
 }
