8853b4b693816fe6c4814525b23ce65d83bc93d2
Sabrina Runge <thesabrinarunge@gmail.com>
2018-07-31 14:23:26 -0500

    DoWhile

---
+++ ch07/DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile
+{
+    public static void main(String[] args)
+    {
+        int num = 100;
+
+        do
+        {
+            System.out.println("DoWhileCountup " + num);
+        } while (num < 10);
+        num += 10;
+
+    }
+}
